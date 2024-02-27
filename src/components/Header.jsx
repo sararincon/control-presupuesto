b406f9de129596ef6c3532cd0a579d61e5930783
Sara Rincón Gonzalez <sararincon@MacBook-Air-de-Sara-2.local>
2024-02-26 22:11:49 -0300

    validando

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import NuevoPresupuesto from "./NuevoPresupuesto";
 
-const Header = ({ presupuesto, setPresupuesto }) => {
+const Header = ({
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto,
+}) => {
   return (
     <header>
       <h1>Planificador de Gastos</h1>
 
-      <NuevoPresupuesto
-        presupuesto={presupuesto}
-        setPresupuesto={setPresupuesto}
-      />
+      {isValidPresupuesto ? (
+        <p>Control de presu</p>
+      ) : (
+        <NuevoPresupuesto
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto} // Nuevo presupuesto no requiere el state pero si la funcion que lo modifica
+        />
+      )}
     </header>
   );
 };
