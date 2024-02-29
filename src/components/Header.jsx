9a64111984ff4a162b41d71b1f3b1a16cc535e07
Sara Rincón Gonzalez <sararincon@MacBook-Air-de-Sara-2.local>
2024-02-28 22:34:18 -0300

    WIP

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NuevoPresupuesto from "./NuevoPresupuesto";
+import ControlPresupuesto from "./ControlPresupuesto";
 
 const Header = ({
   presupuesto,
@@ -12,7 +13,7 @@ const Header = ({
       <h1>Planificador de Gastos</h1>
 
       {isValidPresupuesto ? (
-        <p>Control de presu</p>
+        <ControlPresupuesto presupuesto={presupuesto} />
       ) : (
         <NuevoPresupuesto
           presupuesto={presupuesto}
